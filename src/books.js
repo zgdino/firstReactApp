45fe97d030b102292fb38c43a268acf6820adb84
zgdino <42398905+zgdino@users.noreply.github.com>
2021-07-07 20:55:36 -0700

    Basic react app
    Uploading the most basic app ever!!!

---
+++ src/books.js
@@ -0,0 +1,20 @@
+export const data = [
+{   id:1,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg',
+    title: 'I love you to the moon and back',
+    author: 'Amelia Hepworth'
+},
+
+{   id:2,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/71R8H1PuNdS._AC_UL200_SR200,200_.jpg',
+    title: 'Speechless: Controlling Words, Controlling Minds',
+    author: 'Michael Knowles'
+},
+
+{   id:3,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/813QkLg2LaL._AC_UL200_SR200,200_.jpg',
+    title: 'People We Meet on Vacation',
+    author: 'Emily Henry '
+}
+
+];
--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDom from 'react-dom';
+
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import {data} from './books'
+import SpecificBook from './Book'
+import {greeting} from './testing/testing'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function BookList() {
+    console.log(greeting);
+    return ( 
+        <section className = 'booklist'>
+            {data.map((book, index) => {
+
+                return (
+                    <SpecificBook key = {book.id} {...book}></SpecificBook>
+                )
+            })}
+        </section>
+    );
+ };
+
+ 
+
+ReactDom.render(<BookList />, document.getElementById('root'));
